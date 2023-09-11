d105d1319ec535e90b68921624e34b13f783dc50
amoguelk <amoguelk@hotmail.com>
2023-09-11 10:42:45 -0600

    Added PrintRequest middleware

--- djangoandrea/settings.py
+++ djangoandrea/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "restaurant.middleware.PrintRequest",
 ]
 
 ROOT_URLCONF = "djangoandrea.urls"
