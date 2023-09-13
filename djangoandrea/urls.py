45915be26ef05afe29c72379acdb6a92decaf87e
amoguelk <amoguelk@hotmail.com>
2023-09-13 10:40:38 -0600

    Added image field to menu items

--- djangoandrea/urls.py
+++ djangoandrea/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
 
 urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path("restaurant/", include("restaurant.urls")),
     prefix_default_language=False,
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
